5e5bfe35722a76fbd2d5226ffeb0ff5390fcd5c3
Amrit Bhogal <ambhogal01@gmail.com>
2024-03-06 23:09:28 -0800

    remove test lua

--- test.lua
+++
@@ -1,4 +0,0 @@
-local easyhttp = require("easyhttp")
-local request = assert(easyhttp.async_request("https://njfenjerfnooerfoiernobfoberfboeoibfreboreffrbijoburevbouev.com"))
-local response, code = request:response()
-print(response, code)
